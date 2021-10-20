c7834c814f3f92aae4d7081053fa5d31d02ce1f0
chernykovv <chernykovv@gmail.com>
2021-10-20 12:38:59 -0700

    fix reloading issue

--- app/currency_conversion.rb
+++ app/currency_conversion.rb
@@ -1,8 +1,10 @@
 require 'sinatra/base'
+require "sinatra/reloader"
 require_relative './routes/base'
 require_relative './models/project'
 
 class CurrencyConversion < Sinatra::Base
+  register Sinatra::Reloader
   get '/' do
     'Welcome to the currency conversion application'
   end
--- config.ru
+++ config.ru
@@ -1,12 +1,8 @@
 require 'bundler'
 Bundler.require
 require 'sinatra'
-require 'sinatra/reloader'
 require 'sinatra/activerecord'
 
-configure :development do
-  enable :reloader
-end
 require_relative './app/currency_conversion'
 
 run CurrencyConversion.new
