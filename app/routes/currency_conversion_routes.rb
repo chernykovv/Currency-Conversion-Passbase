4bb3f753eefc4896090a5f68034242935f68aa73
chernykovv <chernykovv@gmail.com>
2021-10-20 13:23:49 -0700

    add key authentication on rates url

--- app/routes/currency_conversion_routes.rb
+++ app/routes/currency_conversion_routes.rb
@@ -1,19 +1,21 @@
 module CurrencyConversionRoutes
   def self.extended(base)
     base.class_exec do
-      get '/rates' do
-        uri = URI.parse('http://data.fixer.io/api/latest?access_key=80e45d5daffd7cda7ee82219248c2f1a')
-        response = JSON.parse(Net::HTTP.get(uri))
-        rates = response['rates']
-        'Euro:' + rates['EUR'].to_s + ' and US dollar:' + rates['USD'].to_s
-      end
-      before '/rates/conversion' do
+      before '/rates' do
         key = params[:key]
         project = Project.find_by(api_key: key)
         if key.nil? || project.nil?
           halt 401, {'Content-Type' => 'text/plain'}, "Sorry your api key doesn't match"
         end
       end
+
+      get '/rates' do
+        uri = URI.parse('http://data.fixer.io/api/latest?access_key=80e45d5daffd7cda7ee82219248c2f1a')
+        response = JSON.parse(Net::HTTP.get(uri))
+        rates = response['rates']
+        'Euro:' + rates['EUR'].to_s + ' and US dollar:' + rates['USD'].to_s
+      end
+
       post '/rates/conversion' do
         uri = URI.parse('http://data.fixer.io/api/latest?access_key=80e45d5daffd7cda7ee82219248c2f1a')
         response = JSON.parse(Net::HTTP.get(uri))
